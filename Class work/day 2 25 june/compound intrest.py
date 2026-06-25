6c5ffedbe2fd885d739a3246d3aef7a488284fd7
imdipanshusharma <vanshpandit2003@gmail.com>
2026-06-25 14:31:36 +0530

    day2

---
+++ Class work/day 2 25 june/compound intrest.py
@@ -0,0 +1,14 @@
+# Program to calculate Compound Interest
+
+P = float(input("Enter the Principal amount: "))
+R = float(input("Enter the Annual Interest Rate (%): "))
+T = float(input("Enter the Time (in years): "))
+
+# Compound Amount
+A = P * (1 + R / 100) ** T
+
+# Compound Interest
+CI = A - P
+
+print("Compound Amount =", round(A, 2))
+print("Compound Interest =", round(CI, 2))
