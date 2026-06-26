6c4a3c3dc0bee07c926d486a0d5e3c149de63e3a
imdipanshusharma <vanshpandit2003@gmail.com>
2026-06-26 21:25:41 +0530

    online shopping

--- Class work/Arthemitic_operator_24 june/Online_Shopping_Discount.py
+++ Class work/Arthemitic_operator_24 june/Online_Shopping_Discount.py
@@ -2,4 +2,5 @@
 product_price = float(input("Enter product price: "))
 discount_amount = float(input("Enter discount amount: "))
 print ("Your product price is = ",product_price)
-print 
+print ("Your discount amount is = ",discount_amount)
+print ("Your final price is = ",product_price - discount_amount)
