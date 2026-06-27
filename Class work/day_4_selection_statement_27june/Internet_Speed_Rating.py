cefbe7043c2b60c63b940ec6fa0dde35e568536b
imdipanshusharma <vanshpandit2003@gmail.com>
2026-06-27 18:13:12 +0530

    Internet_Speed_Rating

---
+++ Class work/day_4_selection_statement_27june/Internet_Speed_Rating.py
@@ -0,0 +1,10 @@
+"""Problem Statement 
+An Internet Service Provider categorizes connection quality based on download speed. 
+• Less than 25 Mbps → Slow  
+• 25 - 99 Mbps → Good  
+• 100 Mbps or above → Excellent  
+Write a Python program to display the connection quality. 
+Sample Input 
+120 
+Sample Output 
+Excellent Connection"""
