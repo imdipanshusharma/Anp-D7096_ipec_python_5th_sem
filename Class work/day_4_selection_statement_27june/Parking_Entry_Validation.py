ad302bf5b203c3adb1d5b87901cf53de92192d5d
imdipanshusharma <vanshpandit2003@gmail.com>
2026-06-27 18:15:02 +0530

    Parking_Entry_Validation

---
+++ Class work/day_4_selection_statement_27june/Parking_Entry_Validation.py
@@ -0,0 +1,33 @@
+"""-----------------Problem Statement------------------------------
+Only vehicles having a valid parking pass are allowed to enter a private parking area. 
+Accept either 1 (Valid Pass) or 0 (No Pass). 
+• If the input is 1, display: 
+Entry Allowed 
+• Otherwise display: 
+Entry Denied 
+------------------------------------------------------------------
+Sample Input 
+0 
+-------------------------------------------------------------------
+Sample Output 
+Entry Denied 
+"""
+#------------------------------Coding-----------------------------------
+# Parking Entry Validation
+print("---------------------------------------------------------------")
+parking_pass = int(input("Enter 1 for Valid Pass or 0 for No Pass: "))
+if parking_pass !=1 and parking_pass !=0:
+    exit("Only enter 1 and 0")
+#------------------------Condition check---------------------------------
+if parking_pass == 1:
+    print("Entry Allowed")
+else:
+    print("Entry Denied")
+
+print("---------------------------------------------------------------")
+"""Output :
+---------------------------------------------------------------
+Enter 1 for Valid Pass or 0 for No Pass: 1
+Entry Allowed
+---------------------------------------------------------------
+"""
