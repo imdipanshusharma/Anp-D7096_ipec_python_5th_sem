e33917bd8a7a439a67d907cfdb6e3263395d61b9
imdipanshusharma <vanshpandit2003@gmail.com>
2026-06-29 11:54:29 +0530

    program to calculate shoping bill

---
+++ Class work/day5_29june_iteration/shoping_cart_billing_section.py
@@ -0,0 +1,52 @@
+"""
+--------------------------------Problem Statement--------------------------------------------------------
+A supermarket allows a customer to purchase multiple products. 
+The customer first enters the number of products. 
+---------------------------------------------------------------
+For each product, enter: 
+* Product Name  
+* Quantity  
+* Price per Unit 
+------------------------------------------------------------------- 
+Finally display: 
+* Individual Product Cost  
+* Total Bill Amount  
+* Most Expensive Product  
+* Cheapest Product  
+* Average Product Cost
+"""
+#---------------------------------------------------------------------------
+#--------------------------------coding section----------------------------------
+#user can enter the number of products
+n = int(input("Enter Number of Products: "))
+
+total_bill = 0
+
+for i in range(1, n + 1):
+
+    name = input("Product Name: ")
+    quantity = int(input("Quantity: "))
+    price = float(input("Price per Unit: "))
+
+    cost = quantity * price
+
+    print("Product Cost =", cost)
+
+    total_bill += cost
+
+    if i == 1:
+        highest_cost = lowest_cost = cost
+        expensive = cheap = name
+    else:
+        if cost > highest_cost:
+            highest_cost = cost
+            expensive = name
+
+        if cost < lowest_cost:
+            lowest_cost = cost
+            cheap = name
+
+print("\nTotal Bill =", total_bill)
+print("Most Expensive Product =", expensive)
+print("Cheapest Product =", cheap)
+print("Average Product Cost =", total_bill / n)
