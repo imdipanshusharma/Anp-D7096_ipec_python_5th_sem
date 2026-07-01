d5231b1c8591ec2aae1953c769261eef64cf8203
imdipanshusharma <vanshpandit2003@gmail.com>
2026-07-01 10:27:47 +0530

    program to remove element from list

---
+++ Class work/day_6_1july_list/list_element_remove.py
@@ -0,0 +1,27 @@
+# Create an empty list
+numbers = []
+# Input elements into the list
+for i in range(10):
+    #input of element from user 
+    num = int(input("Enter element : "))
+    #inserting data in list by 
+    numbers.append(num)
+
+x=int(input("Enter index of the element to remove :"))
+numbers.pop(x)
+print(numbers)
+""""""
+
+'''
+Enter element : 25
+Enter element : 36
+Enter element : 85
+Enter element : 75
+Enter element : 13
+Enter element : 65
+Enter element : 85
+Enter element : 78
+Enter element : 55
+Enter element : 25
+Enter index of the element to remove :2
+[25, 36, 75, 13, 65, 85, 78, 55, 25]'''
