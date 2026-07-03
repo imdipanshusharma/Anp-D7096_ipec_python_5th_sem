e06de5233f3c3995727a431e3ec6c652b0057dd5
imdipanshusharma <vanshpandit2003@gmail.com>
2026-07-03 10:32:31 +0530

    find_odd_no_in_tuple

--- Class work/day_7_tuple/find_odd_no_in_tuple.py
+++ Class work/day_7_tuple/find_odd_no_in_tuple.py
@@ -22,3 +22,32 @@ for i in t:
 
         # Print the odd number
         print(i)
+
+
+        '''Enter Number: 25
+Enter Number: 52
+Enter Number: 25
+Enter Number: 25
+Enter Number: 25
+Enter Number: 05
+Enter Number: 25
+Enter Number: 65
+Enter Number: 68
+Enter Number: 582
+Enter Number: 55
+Enter Number: 6
+Enter Number: 55
+Enter Number: 55456
+Enter Number: 35
+Tuple = (25, 52, 25, 25, 25, 5, 25, 65, 68, 582, 55, 6, 55, 55456, 35)
+Odd Numbers are:
+25
+25
+25
+25
+5
+25
+65
+55
+55
+35'''
