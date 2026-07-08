e2b84033b83f84dd05956ee76930a123cee3f488
imdipanshusharma <vanshpandit2003@gmail.com>
2026-07-08 11:42:36 +0530

    module

---
+++ Class work/8th_july_modules/importing_module.py
@@ -0,0 +1,16 @@
+#importing module
+
+from numeric_calculation  import calculate_addition, calculate_division, calculate_multiplication, calculate_subtraction
+
+
+
+#------------------------------
+a=5
+b=10
+
+print ("sum of", a, "and", b, "is:", calculate_addition(a, b))
+print ("subtraction of", a, "and", b, "is:", calculate_subtraction(a, b))
+print ("multiplication of", a, "and", b, "is:", calculate_multiplication(a, b))
+print ("division of", a, "and", b, "is:", calculate_division(a, b))
+
+
