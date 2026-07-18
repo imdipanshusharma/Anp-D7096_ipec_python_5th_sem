b61906568bd8f98d97cfe129555b3b488d51e8d8
imdipanshusharma <vanshpandit2003@gmail.com>
2026-07-18 11:49:34 +0530

    Delete Class work/day_2_selection_statement_ _25_june/Arthemitic_operator_24 june directory

--- Class work/day_2_selection_statement_ _25_june/Arthemitic_operator_24 june/Online_Shopping_Discount.py
+++
@@ -1,6 +0,0 @@
-# Online Shopping Discount
-product_price = float(input("Enter product price: "))
-discount_amount = float(input("Enter discount amount: "))
-print ("Your product price is = ",product_price)
-print ("Your discount amount is = ",discount_amount)
-print ("Your final price is = ",product_price - discount_amount)
--- Class work/day_2_selection_statement_ _25_june/Arthemitic_operator_24 june/grocery_store_bill.py
+++
@@ -1,7 +0,0 @@
-# Grocery Store Bill Calculation
-price = float(input("Enter price per packet: "))
-packets = int(input("Enter number of packets: "))
-
-total_bill = price * packets
-
-print("Total Bill Amount =", total_bill)
